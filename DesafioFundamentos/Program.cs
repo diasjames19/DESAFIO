0b378d81621009b25bbb5d1e9d799f359715d73a
diasjames19 <diasjames@hotmail.com>
2024-05-03 11:38:59 -0300

    Colocando os Arquivos Projetos

---
+++ DesafioFundamentos/Program.cs
@@ -0,0 +1,57 @@
+﻿using System.ComponentModel;
+using System.Data.Common;
+using DesafioFundamentos.src.model;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {        
+        Cars auto = new Cars();
+       
+         DateTime hoje =  DateTime.Now;
+
+
+
+string opcao = string.Empty;
+bool exibirMenu = true;
+// Realiza o loop do menu
+while (exibirMenu)
+{
+    
+
+    
+    Console.WriteLine($"Data:{hoje}");
+    Console.WriteLine("Digite a sua opção:");
+    Console.WriteLine("1 - Cadastrar veículo");
+    Console.WriteLine("2 - Remover veículo");
+    Console.WriteLine("3 - Listar veículos");
+    Console.WriteLine("4 - Encerrar");
+
+    switch (Console.ReadLine())
+    {
+        case "1":
+            auto.VehicleAdd();
+            break;
+        case "2":
+            auto.VehicleRemove();
+            break;
+        case "3":
+            auto.VehicleList();
+            break;
+        case "4":
+            exibirMenu = false;
+            break;
+        default:
+            Console.WriteLine("Opção inválida");
+            break;
+    }
+    Console.WriteLine("Pressione uma tecla para continuar");
+    Console.ReadLine();
+}
+
+Console.WriteLine("O programa se encerrou");
+
+
+       }   
+    }
+
---
+++ DesafioFundamentos/src/model/Vehicles.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DesafioFundamentos.src.model
+{
+
+     /* 
+            type, model, color, plate, brand
+        
+        */
+    public  abstract class Vehicles
+    {
+
+        public Vehicles(){}
+        public Vehicles(string VehicleModel, string Vehicleplate,  string VehicleColor, DateTime VehicleBegin){
+            
+          this.VehicleModel = VehicleModel;
+          this.Vehicleplate = Vehicleplate;
+          this.VehicleColor = VehicleColor;
+          this.VehicleBegin = VehicleBegin;
+
+        }
+
+     // private decimal StartingPrice = 0;
+      // private decimal TimePrice = 0;
+      
+      public string? VehicleModel{get; set;} 
+      public string? Vehicleplate{get; set;}
+      public string? VehicleColor{get; set;}
+
+        public DateTime VehicleBegin { get; set;}
+      public List<Vehicles> auto = new();  
+
+      public virtual void VehicleAdd(){}
+
+      public virtual void VehicleRemove(){}
+        public virtual void ListVehicles(){}
+    }
+}
